3478ac2586d0ebb85ba206bd61d7c3aec756e9bb
harshalgaydhankar <harshal.gaydhankar@axisbank.com>
2021-07-08 11:38:58 +0530

    Harshal | returns sum of all no saperated by default delimiter

--- src/main/java/com/kata/calculator/StringCalculator.java
+++ src/main/java/com/kata/calculator/StringCalculator.java
@@ -1,10 +1,14 @@
 package com.kata.calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
+
+    String DEFAULT_DELIMITER = ",";
 
     public int add(String input) {
         if(input.isEmpty())
             return 0;
-        return Integer.parseInt(input);
+        return Arrays.stream(input.split(DEFAULT_DELIMITER)).mapToInt(Integer::parseInt).sum();
     }
 }
--- src/test/java/com/kata/calculator/StringCalculatorTest.java
+++ src/test/java/com/kata/calculator/StringCalculatorTest.java
@@ -18,4 +18,13 @@ class StringCalculatorTest {
         assertEquals(3, stringCalculator.add("3"));
     }
 
+    @Test
+    public void shouldReturnSumOfTwoNumbersProvidedUsingDefaultDelimiter() {
+        assertEquals(7, stringCalculator.add("2,5"));
+    }
+
+    @Test
+    public void shouldReturnSumOfMoreNumbersProvidedUsingDefaultDelimiter() {
+        assertEquals(25, stringCalculator.add("1,3,5,7,9"));
+    }
 }
